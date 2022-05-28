d94aebea017d7a76db7b1a09c2279daa4a61e568
muralitharan <muralitharan805@gmail.com>
2022-05-28 13:45:21 +0530

    routing taks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CareersComponent } from './careers/careers.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'aboutus',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'careers',
+    component: CareersComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
